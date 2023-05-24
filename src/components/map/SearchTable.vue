a1da95816ff1633cd0e66a76182cb221897629ee
박영서 <pj0642@gmail.com>
2023-05-24 15:01:28 +0900

    FEAT : swal 적용, 로그인 중이 아닐 때 관심 버튼 누를 때 alert

--- src/components/map/SearchTable.vue
+++ src/components/map/SearchTable.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="px-3 py-2">
-        <div> 
+        <div>
             <b-icon-heart-fill v-if="isInInterests" @click="toggleInterest"></b-icon-heart-fill>
             <b-icon-heart v-else @click="toggleInterest"></b-icon-heart>
         </div>
@@ -39,7 +39,8 @@ export default ({
             this.interests.forEach(elem => {
                 if (elem.aptCode === this.aptCode) cnt++;
             });
-            return (cnt > 0);
+            if (cnt > 0) return true;
+            return false;
         },
         ...mapState(memberStore, ["isLogin", "isLoginError", "userInfo"]),
         ...mapState(dealStore, ["dealList"]),
@@ -48,9 +49,27 @@ export default ({
     methods : {
         ...mapActions(interestStore, ["insert", "del", "getStatList", "getInterestDTOList"]),
         toggleInterest() {
-            let isInInterests = this.isInInterests;
-            if (isInInterests) this.del({aptCode : this.aptCode, userInfo : this.userInfo});
-            else this.insert({aptCode : this.aptCode, userInfo : this.userInfo}); 
+            if (!this.isLogin) {
+                this.$swal({
+                    title: '로그인 필요',
+                    text: "로그인이 필요한 기능입니다.",
+                    type: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Log In',
+                })
+                .then((result) => {
+                    if (result.value) {
+                        this.$router.push('/user/login');
+                    }
+                });
+            }
+            else {
+                let isInInterests = this.isInInterests;
+                if (isInInterests) this.del({aptCode : this.aptCode, userInfo : this.userInfo});
+                else this.insert({aptCode : this.aptCode, userInfo : this.userInfo}); 
+            }
         }
     }
     
